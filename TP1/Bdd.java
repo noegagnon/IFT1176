82abc52117cdccae1610f9b714906daef49d68cf
noegagnon <noemiegagnon23@gmail.com>
2020-06-03 19:41:11 -0400

    addJeu fonctionne

--- TP1/Bdd.java
+++ TP1/Bdd.java
@@ -3,21 +3,43 @@ import java.io.*;
 
 
 public class Bdd implements TestInterface{
-	
+	private Map<String, TreeSet<Jeu>> jeuxVideo;
+	private TreeSet<Jeu> ensemble = new TreeSet<Jeu>(); 
 
     public Bdd() {
-     //a completer
+		jeuxVideo = new LinkedHashMap<>();
     }
 
 
     public void addJeu(Jeu unJeu){
-	// a completer
-		Jeu.add(unJeu);
-
+		System.out.println(unJeu.getTitre());
+		ensemble.add(unJeu);
+		jeuxVideo.put(unJeu.getFabricant(), ensemble);
+		System.out.println(jeuxVideo);
+		//jeuxVideo.add(unJeu.getFabricant, );
+		//if(unJeu.getTitre())
+		/*
+		// si la map ne contient pas le jeu a ajouter, on l'ajoute avec la collection de console vide.
+		if(!jeuxVideo.containsValue(unJeu)) {
+			LinkedHashSet<String> consoles = new LinkedHashSet<String>();
+			//jeuxVideo.put(unJeu, consoles);
+			System.out.println(jeuxVideo);
+			//System.out.println("heelo");
+		
+		}
+*/
     }
 
 	public Jeu getJeu(String titre, String fabricant){
-//a completer et changer l'instruction du return
+		/*
+		Jeu aTrouver = new Jeu(fabricant, titre);
+		System.out.println(aTrouver);
+		if(jeuxVideo.containsKey(aTrouver)) {
+			System.out.println("hey");
+		
+		}
+		*/
+		//a completer et changer l'instruction du return
 		return null;
 	}
 
@@ -43,6 +65,37 @@ public class Bdd implements TestInterface{
 
 	public void saveBdd(String nomFichier){
 	//A completer
+	/*
+		// Creation d'un fichier
+	static void creerFichier(Nation[] pays, int nbPays, int continentVoulu, String nomACreer) 
+			throws IOException
+	{
+		boolean probleme = false;
+		FileWriter fw = null;
+		try
+		{
+			fw = new FileWriter(nomACreer);
+		} catch(java.io.FileNotFoundException erreur)
+		{
+			System.out.println("Probleme pour preparer l'ecriture\n");
+			probleme = true;
+		}
+		if(!probleme)
+		{
+			System.out.println("Debut de la creation du fichier " + nomACreer + "\n");
+			PrintWriter aCreer = new PrintWriter(fw);
+			
+			for(int i=0; i<nbPays; i++)
+			{
+				if(pays[i].getCodeContinent() == continentVoulu)
+					aCreer.printf("%s\n", pays[i]);
+			}
+			
+			aCreer.close();
+			System.out.println("Fin de la creation du fichier " + nomACreer + "\n\n");
+		}
+	}
+	*/
 	}
 
 }
