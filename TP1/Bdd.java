224f065fb9987a17b2debd460459e8fd35be753e
noegagnon <noemiegagnon23@gmail.com>
2020-06-08 21:37:42 -0400

    manque juste addBdd

--- TP1/Bdd.java
+++ TP1/Bdd.java
@@ -1,26 +1,21 @@
 import java.util.*;
 import java.io.*;
 
-// arranger ensemble pour etre juste pour 1 jeu
 public class Bdd implements TestInterface{
-	//private Map<String, TreeSet<Jeu>> jeuxVideo;
-	//private TreeSet<Jeu> ensemble = new TreeSet<Jeu>(); 
-	Map<String, TreeSet<Jeu>> jeuxVideo = new LinkedHashMap<>();
-	//TreeSet<Jeu> ensemble = new TreeSet<Jeu>(); 
-	TreeSet<Jeu> ensemble;
+
+	private Map<String, TreeSet<Jeu>> jeuxVideo = new LinkedHashMap<>();
+	private TreeSet<Jeu> ensemble;
+
     public Bdd() {
 		jeuxVideo = new LinkedHashMap<>();
 	}
 
-
-	//ajouter qu'il faut ajouter les consolessi le jeu est deja present
+	// Ajoute le jeu passe en parametre a la banque de donnees
     public void addJeu(Jeu unJeu){
-
 		// Si le fabricant est dans la base de donnees, on y ajoute le jeu, sinon on cree un ensemble avec 
 		// ce fabricant
 		String fabricant = unJeu.getFabricant();
 		ensemble = jeuxVideo.get(fabricant);
-		//System.out.println(unJeu.getConsoles());
 		String[] uneConsole = new String[unJeu.getConsoles().size()];
 		uneConsole = unJeu.getConsoles().toArray(uneConsole);
 		
@@ -43,26 +38,20 @@ public class Bdd implements TestInterface{
 			ensemble.add(unJeu);
 			jeuxVideo.put(fabricant, ensemble);
 		}	
-		System.out.println(jeuxVideo);
-
     }
 
+	// Retourne le jeu passe en parametre
 	public Jeu getJeu(String titre, String fabricant){
 		Jeu aTrouver = new Jeu(fabricant, titre);
 
 		if(ensemble.contains(aTrouver)) {
 			Iterator<Jeu> it = ensemble.iterator();
-		 	//boolean trouve = false;
-
 		 	while(it.hasNext()) {
 		 		Jeu courant = it.next();
 		 		if (aTrouver.equals(courant)) {
-		 			System.out.println("courant " + courant);
-		 			//trouve = true;
 					return courant;
 		 		}
 		 	}
-
 		} else {
 			System.out.println("Le jeu " + aTrouver.getTitre() + " pas trouve");
 		}
@@ -71,73 +60,33 @@ public class Bdd implements TestInterface{
 
 
 	public void addBdd(String nomFile){
-// a completer
+		// a completer
 	}
 
 	public void loadBdd(String nomFile){
-		//Map<String, TreeSet<Jeu>> jeuxVideo = new LinkedHashMap<>();
-		//TreeSet<Jeu> ensemble;
 
 		FileReader fr = null;
 		boolean existeFile = true;
 		boolean finFichier = false;
 		try {
-		   fr = new FileReader(nomFile);
-		
-
+			fr = new FileReader(nomFile);
 			if (existeFile) {
-			BufferedReader entree = new BufferedReader (fr);
-
-			while (!finFichier) {
-				String ligneLue = entree.readLine(); // null si fin de fichier
-				if (ligneLue != null){
-					//System.out.println(ligneLue);
-					
-					String[] infoJeux = ligneLue.split(";");
-					//System.out.println(infoJeux);
-					
-					String fabricant = infoJeux[0];
-					String titre = infoJeux[1];
-					String cote = infoJeux[2]; 
-					String consoles = infoJeux[3]; 
-					String[] uneConsole = consoles.split(",");
-					
-					Jeu nouv = new Jeu(fabricant, titre, cote, new LinkedHashSet<String>(Arrays.asList(uneConsole)));
-					//System.out.println("nouv " + nouv);
-					// Ajout des consoles au jeu
-					
-
-					// Si le fabricant est dans la base de donnees, on y ajoute le jeu, sinon on cree un ensemble avec 
-					// ce fabricant
-					addJeu(nouv);
-					/*
-					ensemble = jeuxVideo.get(fabricant);
-
-					if(ensemble != null) {
-						//caller addJeu nouv.addJeu();			
-						boolean jeuExiste = false;
-						for(Jeu j : ensemble) {
-
-							if(nouv.equals(j)) {
-								for(int i=0; i<uneConsole.length; i++ ){
-									j.addConsoles(uneConsole[i]);
-									jeuExiste = true;
-								}	
-							} 
-						} 
-						if(!jeuExiste) {
-							ensemble.add(nouv);
-						} 						
-					} else {
-						ensemble = new TreeSet<Jeu>();
-						ensemble.add(nouv);
-						jeuxVideo.put(fabricant, ensemble);
-					}	
-					*/
+				BufferedReader entree = new BufferedReader (fr);
+				while (!finFichier) {
+					String ligneLue = entree.readLine(); // null si fin de fichier
+					if (ligneLue != null){
+						String[] infoJeux = ligneLue.split(";");					
+						String fabricant = infoJeux[0];
+						String titre = infoJeux[1];
+						String cote = infoJeux[2]; 
+						String consoles = infoJeux[3]; 
+						String[] uneConsole = consoles.split(",");
+						Jeu nouv = new Jeu(fabricant, titre, cote, new LinkedHashSet<String>(Arrays.asList(uneConsole)));
+						addJeu(nouv);
+					}
+					else finFichier = true;
 				}
-				else finFichier = true;
-			}
-			entree.close();
+				entree.close();
 			}
 		} catch (java.io.FileNotFoundException e) {
 			System.out.println("Probleme d'ouvrir le fichier " + nomFile);
@@ -145,30 +94,25 @@ public class Bdd implements TestInterface{
 		} catch(IOException e) {
 			System.out.println("Erreur lors de la lecture du fichier");
 		}
-		//System.out.println(jeuxVideo);	 
 	}
 
+	// Retourne les jeu pouvant se jouer sur la console passee en parametre
 	public ArrayList<Jeu> chercheConsole(String console){
-		//System.out.println(jeuxVideo.trouveConsole("EA"));
-	// a completer et changer l'instruction du return
-		//System.out.println("asd" + jeuxVideo);
 		ArrayList<Jeu> gameWithConsole= new ArrayList();
 		Iterator<Jeu> it = ensemble.iterator();
-		//boolean trouve = false;
 
 		while(it.hasNext()) {
 			Jeu jeuCourant = it.next();
 			if(jeuCourant.trouveConsole(console)) {
 				gameWithConsole.add(jeuCourant);
-				//System.out.println("courant " + jeuCourant);
 			}
 
 		}
 		return gameWithConsole;
 	}
 
+	// Retourne les jeu realises par le fabricant passe en parametre
 	public Collection<Jeu> getJeuxFabricant(String fabricant){
-	// a completer et potentiellement changer l'instruction du return
 		Collection<Jeu> jeuxFab = new ArrayList();
 		Iterator<Jeu> it = ensemble.iterator();
 
@@ -176,15 +120,13 @@ public class Bdd implements TestInterface{
 			Jeu jeuCourant = it.next();
 			if(jeuCourant.getFabricant().equalsIgnoreCase(fabricant)) {
 				jeuxFab.add(jeuCourant);
-				//System.out.println("courant " + jeuCourant);
 			}
 		}	
 		return jeuxFab;
 	}
 
-
+	// Retourne les jeu portant la cote passee en parametre
 	public Collection<Jeu> chercheCote(String cote){
-	// a completer et potentiellement changer l'instruction du return
 		Collection<Jeu> jeuxCote = new ArrayList();
 		Iterator<Jeu> it = ensemble.iterator();
 
@@ -194,50 +136,40 @@ public class Bdd implements TestInterface{
 			for(int i=0; i<toutesCotes.length; i++) {
 				if(toutesCotes[i].equalsIgnoreCase(cote)) {
 					jeuxCote.add(jeuCourant);
-					//System.out.println("courant " + jeuxCote);
 				}
 			}
-			//System.out.println(toutesCotes);
-
 		}	
 		return jeuxCote;
 	}
 
+	// Enregistre la banque de donnees dans un fichier dont le nom est passe en parametre
 	public void saveBdd(String nomFichier){	
-		// Creation d'un fichier
-		/*
 		boolean probleme = false;
 		FileWriter fw = null;
 		try
 		{
-			fw = new FileWriter(nomACreer);
-
-		if(!probleme)
-		{
-			System.out.println("Debut de la creation du fichier " + nomACreer + "\n");
-			PrintWriter aCreer = new PrintWriter(fw);
-			
-			for(int i=0; i<nbPays; i++)
+			fw = new FileWriter(nomFichier);
+			if(!probleme)
 			{
-				if(pays[i].getCodeContinent() == continentVoulu)
-					aCreer.printf("%s\n", pays[i]);
+				System.out.println("Debut de la creation du fichier " + nomFichier + "\n");
+				PrintWriter aCreer = new PrintWriter(fw);
+				for(Map.Entry<String, TreeSet<Jeu>> entry : jeuxVideo.entrySet()) {
+					//System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue()); 
+					Iterator<Jeu> it = entry.getValue().iterator();
+					while(it.hasNext()) {
+						Jeu jeuCourant = it.next();
+						//System.out.println("jeucourant " + jeuCourant);
+						aCreer.println(jeuCourant);
+					}					
+				}	
+				aCreer.close();
 			}
-			
-			aCreer.close();
-
 		} catch(java.io.FileNotFoundException erreur){
 			System.out.println("Probleme pour preparer l'ecriture\n");
 			probleme = true;
 		} catch(IOException e) {
 			System.out.println("Erreur lors de l'ecriture du fichier");
 		}
-			System.out.println("Fin de la creation du fichier " + nomACreer + "\n\n");
-		}
-	
-	
-
-*/	
+		System.out.println("Fin de la creation du fichier " + nomFichier + "\n\n");
 	}
-
-
 }
