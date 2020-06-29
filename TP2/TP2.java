8c701c6129526882f8fb86d4c4dd72baf56d07f1
noegagnon <noemiegagnon23@gmail.com>
2020-06-29 19:13:36 -0400

    save Bdd fonctionne

--- TP2/TP2.java
+++ TP2/TP2.java
@@ -9,12 +9,18 @@ import javax.swing.*;
 import java.util.*;
 import java.io.*;
 
-public class TP2 implements ActionListener{
+public class TP2 extends JFrame implements ActionListener{
 
 	private JFrame frame1;
 
-	private JPanel panel;
-	private JPanel panel2;
+	// panel
+	private JPanel currentPanel;
+	private JPanel loadBddPanel = new loadPanel();
+	private JPanel addBddPanel = new addPanel();
+	private JPanel saveBddPanel = new savePanel();
+
+	
+	// menu
 	private TextField tf;
 	private JMenu fichier;
 	private JMenuItem loadBdd;
@@ -34,7 +40,6 @@ public class TP2 implements ActionListener{
 
 	public TP2(int l, int h){
 		setSize(l,h);
-
 		
 		//menu
 		JMenuBar mb = new JMenuBar();
@@ -50,7 +55,7 @@ public class TP2 implements ActionListener{
 		getJeuFabricant = new JMenuItem("get Jeu Fabricant");
 		chercheCote = new JMenuItem("cherche cote");
 
-
+		addJeu.addActionListener(this);
 		fichier.add(loadBdd);
 		fichier.add(addBdd);
 		fichier.add(saveBdd);
@@ -65,46 +70,167 @@ public class TP2 implements ActionListener{
 		mb.add(jeu);
 		
 		frame1 = new JFrame();
+		
+		// boutons
 		JButton buttonLoad = new JButton("Load bdd");
-
 		JButton buttonReset = new JButton("Reset");
-
-
 		buttonLoad.addActionListener(this);
-
 		buttonReset.addActionListener(this);
-		
-		tf = new TextField(10);
-		panel = new JPanel();
-		panel2 = new JPanel();
-		
-		panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-		panel.setLayout(new GridLayout(4,3));
-		panel2.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-		panel2.setLayout(new BorderLayout());
-		
-		panel2.add(tf);
-		panel.add(buttonLoad);
-
-		panel.add(buttonReset);
+			
+		currentPanel = new JPanel();
+		currentPanel.setBorder(BorderFactory.createEmptyBorder(60,60,60,60));
+		currentPanel.add(new JLabel("Veuillez choisir une option dans la barre de menu."));
 		
 		frame1.setJMenuBar(mb);
-
-		frame1.add(panel2, BorderLayout.NORTH);
-		frame1.add(panel, BorderLayout.CENTER);
+		frame1.add(currentPanel, BorderLayout.CENTER);
 		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame1.setTitle("Load une bdd");
+		frame1.setTitle("Menu");
 		frame1.pack();
 		frame1.setVisible(true);
+		frame1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		
+		loadBdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				//frame1.dispose();
+				//new loadBdd();
+                swapPanel(loadBddPanel);
+				
+			}
+		});
 
-		
-		
+		frame1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		saveBdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				//frame1.dispose();
+				swapPanel(saveBddPanel);
+			}
+		});
+
+		frame1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		addBdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				//frame1.dispose();
+				//new addBdd();
+				swapPanel(addBddPanel);
+
+			}
+		});
 
 	}
-	private void setSize(int l, int h) {
+    public class loadPanel extends JPanel implements ActionListener {{
+        //this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setBorder(BorderFactory.createEmptyBorder(60,60,60,60));
+
+		JButton buttonLoad = new JButton("Load bdd");
+		JButton buttonReset = new JButton("Reset");
+		buttonLoad.addActionListener(this);
+		buttonReset.addActionListener(this);
+		
+		tf = new TextField(50);
+		this.add(buttonLoad);
+		this.add(buttonReset);
+		this.add(tf);
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
-	}
+		// tf.getText() ca fait rien, donc fonctionne pas.
+		String text = tf.getText();
+		String action = e.getActionCommand();
+		System.out.println(text);
+		System.out.println("action" + action);
+		if(action == "Load bdd") {
+	    	laBase.loadBdd("jeux.txt");
+		}
+
+		if(action == "Reset") {
+			tf.setText("");
+		} 	
+	}}
+    
+    // la seule qui fonctionne ...
+    public class savePanel extends JPanel implements ActionListener {{
+        //this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setBorder(BorderFactory.createEmptyBorder(60,60,60,60));
+
+		JButton buttonSave = new JButton("Save bdd");
+		JButton buttonReset = new JButton("Reset");
+		buttonSave.addActionListener(this);
+		buttonReset.addActionListener(this);
+		
+		tf = new TextField(50);
+		this.add(buttonSave);
+		this.add(buttonReset);
+		this.add(tf);
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		// tf.getText() ca fait rien, donc fonctionne pas.
+		String text = tf.getText();
+		String action = e.getActionCommand();
+		System.out.println(text);
+		System.out.println("action" + action);
+		if(action == "Save bdd") {
+	    	laBase.saveBdd(text);
+		}
+
+		if(action == "Reset") {
+			tf.setText("");
+		} 	
+	}}
+    
+    public class addPanel extends JPanel implements ActionListener {{
+        //this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setBorder(BorderFactory.createEmptyBorder(60,60,60,60));
+
+		JButton buttonAdd = new JButton("Add bdd");
+		JButton buttonReset = new JButton("Reset");
+		buttonAdd.addActionListener(this);
+		buttonReset.addActionListener(this);
+		
+		tf = new TextField(50);
+		this.add(buttonAdd);
+		this.add(buttonReset);
+		this.add(tf);
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		// tf.getText() ca fait rien, donc fonctionne pas.
+		String text = tf.getText();
+		String action = e.getActionCommand();
+		System.out.println(text);
+		System.out.println("action" + action);
+		if(action == "Add bdd") {
+	    	laBase.addBdd(text);
+		}
+
+		if(action == "Reset") {
+			tf.setText("");
+		} 	
+	}}
+
+    protected void swapPanel(JPanel newPanel) {
+        SwingUtilities.invokeLater(new Runnable() {   	
+            @Override
+            public void run() {
+                frame1.remove(currentPanel);
+                frame1.add(newPanel);
+                currentPanel = newPanel;
+                frame1.invalidate();
+                frame1.revalidate();
+            }
+        });
+    }
+    
+
 	public static void afficherJeu(TestInterface b, String fab, String titre ) 			
 	throws IOException 
 	{
@@ -114,16 +240,20 @@ public class TP2 implements ActionListener{
 		else
 			System.out.println(titre + " n'est pas dans la banque de donnees");
 	}
+	
 	public static void main (String[] args) {
 		new	TP2(300,300);
 	}
 	
+	
+	//pas besoin
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String text = tf.getText();
 		String action = e.getActionCommand();
 		System.out.println(text);
 		System.out.println(action);
+		/*
 		if(action == "Load bdd") {
 	    	laBase.loadBdd(text);
 
@@ -133,5 +263,7 @@ public class TP2 implements ActionListener{
 		if(action == "Reset") {
 			tf.setText("");
 		} 
+		*/
+
 	}
 }
