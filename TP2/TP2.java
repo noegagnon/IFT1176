37df3da87f396e55ce376a155705bb5af597112c
noegagnon <noemiegagnon23@gmail.com>
2020-06-29 21:30:21 -0400

    load bdd, save bdd et get jeu fonctionnent

--- TP2/TP2.java
+++ TP2/TP2.java
@@ -19,9 +19,15 @@ public class TP2 extends JFrame implements ActionListener{
 	private JPanel addBddPanel = new addPanel();
 	private JPanel saveBddPanel = new savePanel();
 
+	private JPanel getJeuPanel = new getJeuPanel();
 	
 	// menu
 	private TextField tf;
+	private TextField bddAAdd;
+	private TextField bddALoad;
+	private TextField fabricant;
+	private TextField titre;
+
 	private JMenu fichier;
 	private JMenuItem loadBdd;
 	private JMenuItem addBdd;
@@ -115,6 +121,15 @@ public class TP2 extends JFrame implements ActionListener{
 
 			}
 		});
+		
+		getJeu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				//frame1.dispose();
+				//new addBdd();
+				swapPanel(getJeuPanel);
+
+			}
+		});
 
 	}
     public class loadPanel extends JPanel implements ActionListener {{
@@ -126,10 +141,10 @@ public class TP2 extends JFrame implements ActionListener{
 		buttonLoad.addActionListener(this);
 		buttonReset.addActionListener(this);
 		
-		tf = new TextField(50);
+		bddALoad = new TextField(50);
 		this.add(buttonLoad);
 		this.add(buttonReset);
-		this.add(tf);
+		this.add(bddALoad);
     }
 
 	@Override
@@ -137,16 +152,16 @@ public class TP2 extends JFrame implements ActionListener{
 		// TODO Auto-generated method stub
 		
 		// tf.getText() ca fait rien, donc fonctionne pas.
-		String text = tf.getText();
+		String text = bddALoad.getText();
 		String action = e.getActionCommand();
 		System.out.println(text);
 		System.out.println("action" + action);
 		if(action == "Load bdd") {
-	    	laBase.loadBdd("jeux.txt");
+	    	laBase.loadBdd(text);
 		}
 
 		if(action == "Reset") {
-			tf.setText("");
+			bddALoad.setText("");
 		} 	
 	}}
     
@@ -193,10 +208,10 @@ public class TP2 extends JFrame implements ActionListener{
 		buttonAdd.addActionListener(this);
 		buttonReset.addActionListener(this);
 		
-		tf = new TextField(50);
+		bddAAdd = new TextField(50);
 		this.add(buttonAdd);
 		this.add(buttonReset);
-		this.add(tf);
+		this.add(bddAAdd);
     }
 
 	@Override
@@ -204,16 +219,16 @@ public class TP2 extends JFrame implements ActionListener{
 		// TODO Auto-generated method stub
 		
 		// tf.getText() ca fait rien, donc fonctionne pas.
-		String text = tf.getText();
+		String strFichier = bddAAdd.getText();
 		String action = e.getActionCommand();
-		System.out.println(text);
+		System.out.println(strFichier);
 		System.out.println("action" + action);
 		if(action == "Add bdd") {
-	    	laBase.addBdd(text);
+	    	laBase.addBdd(strFichier);
 		}
 
 		if(action == "Reset") {
-			tf.setText("");
+			bddAAdd.setText("");
 		} 	
 	}}
 
@@ -230,7 +245,56 @@ public class TP2 extends JFrame implements ActionListener{
         });
     }
     
+    // get Jeu
+    public class getJeuPanel extends JPanel implements ActionListener {{
+        //this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setBorder(BorderFactory.createEmptyBorder(60,60,60,60));
 
+		JButton buttonGetJeu = new JButton("getJeu bdd");
+		JButton buttonReset = new JButton("Reset");
+		buttonGetJeu.addActionListener(this);
+		buttonReset.addActionListener(this);
+		JLabel titreAEcrire = new JLabel("Veuillez entrez le titre du jeu recherche");
+		JLabel fabricantAEcrire = new JLabel("Veuillez entrez le fabricant du jeu recherche");
+		JLabel reponse = new JLabel();
+
+		
+		titre = new TextField(50);
+		fabricant = new TextField(50);
+		this.add(buttonGetJeu);
+		this.add(buttonReset);
+		this.add(titreAEcrire);
+		this.add(titre);
+		this.add(fabricantAEcrire);
+		this.add(fabricant);
+		this.add(reponse);
+
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		// tf.getText() ca fait rien, donc fonctionne pas.
+		String strTitre = titre.getText();
+		String strFabricant = fabricant.getText();
+		
+		String action = e.getActionCommand();
+		System.out.println(titre);
+		System.out.println(fabricant);
+
+		System.out.println("action" + action);
+		if(action == "getJeu bdd") {
+	    	laBase.getJeu(strTitre, strFabricant);
+			//reponse.setText(laBase.getJeu(strTitre, strFabricant));
+	    	// afficher le jeu.. type Jeu pas string
+		}
+
+		if(action == "Reset") {
+			titre.setText("");
+			fabricant.setText("");
+		} 	
+	}}
 	public static void afficherJeu(TestInterface b, String fab, String titre ) 			
 	throws IOException 
 	{
