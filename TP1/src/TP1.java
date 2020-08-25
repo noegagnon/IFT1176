7105e2a2b45be6775a37f21b93db0a5e4057bc15
noegagnon <noemiegagnon23@gmail.com>
2020-08-25 16:20:53 -0400

    final

--- TP1/src/TP1.java
+++ TP1/src/TP1.java
@@ -22,42 +22,41 @@ public class TP1 {
 	}
 
 
-    public static void main(String[] args) throws IOException
- 	{
-    	TestInterface laBase = new Bdd();
-    	Jeu unJeu;
+	public static void main(String[] args) throws IOException
+	{
+		TestInterface laBase = new Bdd();
+		Jeu unJeu;
 
-    	unJeu = new Jeu("EA", "The Sims 5", "M");
-    	laBase.addJeu(unJeu);
+		unJeu = new Jeu("EA", "The Sims 5", "M");
+		laBase.addJeu(unJeu);
 
-    	unJeu.addConsole("PS4");
-    	unJeu.addConsole("XONE");
+		unJeu.addConsole("PS4");
+		unJeu.addConsole("XONE");
 
-    	System.out.println("Les infos sur les Sims 5 : ");
-    	afficherJeu(laBase, "EA", "The Sims 5");
+		System.out.println("Les infos sur les Sims 5 : ");
+		afficherJeu(laBase, "EA", "The Sims 5");
 
-    	laBase.loadBdd("jeux.txt");
+		laBase.loadBdd("jeux.txt");
 
-    	System.out.println("\n\nApr�s le load, les infos sur les Sims 5 : ");
-    	afficherJeu(laBase, "EA", "The Sims 5");
+		System.out.println("\n\nApr�s le load, les infos sur les Sims 5 : ");
+		afficherJeu(laBase, "EA", "The Sims 5");
 
-    	System.out.println("Les infos sur NHL 20 : ");
-    	afficherJeu(laBase, "EA", "NHL 2020");
+		System.out.println("Les infos sur NHL 20 : ");
+		afficherJeu(laBase, "EA", "NHL 2020");
 
-    	System.out.println("Les infos sur Vampyr : ");
-    	afficherJeu(laBase, "FOCUS", "Vampyr");
-
+		System.out.println("Les infos sur Vampyr : ");
+		afficherJeu(laBase, "FOCUS", "Vampyr");
 
 		laBase.addBdd("jeuxComplement.txt");
 
 		System.out.println("\n\nApr�s le addBdd, les infos sur les Sims 5 : ");
-    	afficherJeu(laBase, "EA", "The Sims 5");
+		afficherJeu(laBase, "EA", "The Sims 5");
 
-    	System.out.println("Les infos sur NHL 20 : ");
-    	afficherJeu(laBase, "EA", "NHL 2020");
+		System.out.println("Les infos sur NHL 20 : ");
+		afficherJeu(laBase, "EA", "NHL 2020");
 
-    	System.out.println("Les infos sur Vampyr : ");
-    	afficherJeu(laBase, "FOCUS", "Vampyr");
+		System.out.println("Les infos sur Vampyr : ");
+		afficherJeu(laBase, "FOCUS", "Vampyr");
 
 		System.out.println("\n\nLes jeux disponibles sur la SWITCH sont :");
 		List<Jeu> lstSwitch = laBase.chercheConsole("SWITCH");
